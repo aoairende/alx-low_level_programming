23b483a412aa789b56d43164eb3b377c3bd4671b
aoairende <aoairende@gmail.com>
2023-02-17 13:40:55 -0800

    C-Varaiables, if, else, while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Write a C program that assigns a random number to the varaiable
+ * (n) each time it is executed.Complete the source code in order to print
+ * whether the number is stored in the varaiable (n) is +ve or -ve.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+
