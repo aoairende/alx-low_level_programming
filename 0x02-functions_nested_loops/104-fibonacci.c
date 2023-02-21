4b6e6fadd5dc8515ea445a35423ab5d053088ee8
aoairende <aoairende@gmail.com>
2023-02-21 15:55:59 -0800

    Last task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,13 +2,11 @@
 
 /**
  * main - Entry point
- *
  * Description: Write a program that finds and prints the first
  * 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	int count;
@@ -34,7 +32,6 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
