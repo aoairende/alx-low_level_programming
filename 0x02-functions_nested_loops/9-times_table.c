223566295c7faf6b5d0bde89f1a4c62f797a6a71
aoairende <aoairende@gmail.com>
2023-02-21 10:35:11 -0800

    Leaen your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,8 +27,8 @@ void times_table(void)
 				_putchar(' ');
 
 			else
-				_putchar((prod / 10) + '0');
-			
+				_putchar((prod / 10) + '0'vi
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
