3879355923c5497c3efff8ed35f989ffb6ebdb7f
aoairende <aoairende@gmail.com>
2023-02-23 01:39:40 -0800

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Copy this main file. Comment out (don’t delete it!)
+ * the part of the code that is causing the output to go into an
+ * infinite loop.
+ * Don’t add or remove any lines of code, as we will be checking
+ * your line count. You are only allowed to comment out existing code.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
