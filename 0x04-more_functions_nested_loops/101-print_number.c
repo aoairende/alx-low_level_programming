f4f73b530367ca93dcb5a4876fca34ff04578313
aoairende <aoairende@gmail.com>
2023-02-23 17:47:41 -0800

    advanced task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n:.input integer parameter
+ *
+ * Description: print an integer
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+
+	_putchar(i % 10 + '0');
+}
