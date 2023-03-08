8630406b76c99d6da8724e62f1e02f25deecec15
aoairende <aoairende@gmail.com>
2023-03-08 00:51:17 -0800

    print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ *
+ * Description: print a string in reverse
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
