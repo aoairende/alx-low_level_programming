6c199e09f72839f04a82ebb024315d04a1d6c22f
aoairende <aoairende@gmail.com>
2023-04-05 17:50:24 -0700

    singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,17 @@
  *
  * Return: void
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *next;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->temp;
-		free(*temp);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
-	*head = NULL;
 }
