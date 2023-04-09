86288f94b5bdba759f7d6e401c4c232ee0b9f48f
aoairende <aoairende@gmail.com>
2023-04-09 16:07:52 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,24 @@
  * Return: the converted number
  */
 
-unsigned int binary_to_uint(const char *b) {
-    unsigned int result = 0;
-    if (b == NULL) {
-        return 0;
-    }
-    for (int i = 0; b[i] != '\0'; i++) {
-        if (b[i] == '0' || b[i] == '1') {
-            result = result << 1;
-            result += b[i] - '0';
-        } else {
-            return 0;
-        }
-    }
-    return result;
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int j;
+
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
