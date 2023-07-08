8b0ac75fde14608d0edb63bb5bc169ec6f247569
Abraham Airende <aoairende@gmail.com>
2023-07-07 19:40:44 -0700

    1.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -33,25 +33,25 @@ unsigned long int _power(unsigned int base, unsigned int pow)
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int dev, result;
-	char flag;
+	unsigned long int s, t;
+	char u;
 
-	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+	u = 0;
+	s = _power(2, sizeof(unsigned long int) * 8 - 1);
 
-	while (dev != 0)
+	while (s != 0)
 	{
-		result = n & dev;
-		if (result == dev)
+		t = n & s;
+		if (t == s)
 		{
-			flag = 1;
+			u = 1;
 			_putchar('1');
 
 		}
-		else if (flag == 1 || dev == 1)
+		else if (u == 1 || s == 1)
 		{
 			_putchar('0');
 		}
-		dev >>= 1;
+		s >>= 1;
 	}
 }
