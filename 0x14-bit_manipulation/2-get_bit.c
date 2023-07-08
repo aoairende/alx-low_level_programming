0417d4fd1c4078f58de5a67c501048944d337941
Abraham Airende <aoairende@gmail.com>
2023-07-07 19:52:28 -0700

    2.10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_bit - Entry point.
+ *
+ * Description: Write a function that returns the value of a bit at a
+ * given index.
+ *
+ * @n: To retrieve bits.
+ *
+ * @index: To check position of the bits.
+ *
+ * Return: The value of the bit at the specified index.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int x, y;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	x = 1 << index;
+	y = n & div;
+	if (y == x)
+		return (1);
+
+	return (0);
+}
