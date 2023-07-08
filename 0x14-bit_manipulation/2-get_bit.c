93f959b039a7ac470d820635065a1ad8bbd01e8b
Abraham Airende <aoairende@gmail.com>
2023-07-07 19:54:24 -0700

    2.10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -20,7 +20,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	x = 1 << index;
-	y = n & div;
+	y = n & x;
 	if (y == x)
 		return (1);
 
