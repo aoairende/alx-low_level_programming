7caf6172a3aac92e479d65a3b2903c2ba70729b8
Abraham Airende <aoairende@gmail.com>
2023-07-10 11:39:05 -0700

    Thread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t open, read, write;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
@@ -28,18 +28,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	open = open(filename, O_RDONLY);
-	read = read(o, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, r);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (open == -1 || read == -1 || write == -1 || write != read)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(open);
+	close(o);
 
-	return (write);
+	return (w);
 }
