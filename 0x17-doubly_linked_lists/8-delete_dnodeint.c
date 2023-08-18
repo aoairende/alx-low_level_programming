c28fca59ea2ebb05bd38db2608b67de68b70d390
Abraham Airende <aoairende@gmail.com>
2023-08-17 19:46:16 -0700

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	/* Check for invalid cases: NULL head or an empty list. */
 	if (!head || !current_node)
 		return (-1);
-
 	/* Traverse to the desired index or the end. */
 	while (current_index != index && current_node)
 	{
@@ -26,7 +25,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current_node = current_node->next;
 		current_index++;
 	}
-
 	/* If the desired index is out of bounds, return -1. */
 	if (!current_node)
 		return (-1);
@@ -50,7 +48,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		previous_node->next = current_node->next;
 		current_node->prev = NULL;
 	}
-
 	/* Free the memory occupied by the deleted node. */
 	free(current_node);
 	return (1);
