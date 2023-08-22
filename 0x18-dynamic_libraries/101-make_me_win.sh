a5b0462f9ea361315331674184f3a84614731252
Abraham Airende <aoairende@gmail.com>
2023-08-21 19:48:01 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/aoairende/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/aoairende/alx-low_level_programming/7a570cea3404488c3a0d384905dd36da07e48137/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
