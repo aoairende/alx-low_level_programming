a41857da5846c494b7d8fd42b1cabfe2cca70a1e
aoairende <aoairende@gmail.com>
2023-09-08 01:22:03 +0100

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints the contents of a hash table.
+ * 
+ * @ht: A pointer to the hash table.
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	int first_print = 1;
+	hash_node_t *node = NULL;
+
+    /* Check if the hash table pointer is NULL. */
+	if (!ht)
+		return;
+	printf("{");
+    /* Iterate through the hash table's array. */
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+
+		while (node)
+		{
+			if (first_print)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				first_print = 0;
+			}
+			else
+			{
+				printf(", '%s': '%s'", node->key, node->value);
+			}
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
