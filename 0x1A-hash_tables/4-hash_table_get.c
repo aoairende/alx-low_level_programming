ca85f0fc6335f5b2698eb9478f72b15ee2e8cdb5
aoairende <aoairende@gmail.com>
2023-09-08 01:20:07 +0100

    >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key from a hash table.
+ * 
+ * @ht: A pointer to the hash table.
+ * @key: A pointer to the key.
+ * 
+ * Return: The value associated with the given key if found, otherwise NULL.
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *current_node = NULL;
+
+    /* Check if the hash table pointer, key, or key length is invalid. */
+	if (!ht || !key || strlen(key) == 0)
+		return (NULL);
+
+    /* Calculate the index where the key is likely to be found. */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* check if a node in the list has same key and return value */
+	current_node = ht->array[index];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			return (current_node->value);
+		}
+		current_node = current_node->next;
+	}
+    /* If the key is not found in the linked list, return NULL. */
+	return (NULL);
+}
